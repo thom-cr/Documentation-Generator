0f607ea6fb8dbf98beb27ad2c49d17529e8cc869
Thomas CRATER <CraterT@ad.fms-ea.com>
2025-01-22 08:56:37 +0100

    Add function WritePDF and move js to jsx

--- src/pages/components/CheckBoxOutputFormat.jsx
+++ src/pages/components/CheckBoxOutputFormat.jsx
@@ -21,7 +21,7 @@ const CheckBoxOutputFormat = ({ formData, setFormData }) => {
                         Format de sortie :
                     </Typography>
                 </Box>
-                <FormControlLabel control={<Checkbox sx={{ border: '10px', borderColor: 'white' }} checked={formData.formats?.pdf || false} onChange={handleCheckboxChange} name='pdf'/>} label="PDF" />
+                <FormControlLabel control={<Checkbox sx={{}} checked={formData.formats?.pdf || false} onChange={handleCheckboxChange} name='pdf'/>} label="PDF" />
                 <FormControlLabel control={<Checkbox checked={formData.formats?.markdown || false} onChange={handleCheckboxChange} name='markdown'/>} label="MarkDown" />
             </Box>
         </>
